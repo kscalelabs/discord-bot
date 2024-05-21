008fee9d29e25a4e103369d2f1ab2d9fe1c3e1d4
Benjamin Bolte <ben@bolte.cc>
2024-05-20 17:32:03 -0700

    format / fix lint

--- stompy/main.py
+++ stompy/main.py
@@ -9,7 +9,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 load_dotenv()
-TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
+TOKEN: Final[str] = os.getenv("DISCORD_TOKEN")
 
 intents: Intents = Intents.default()
 intents.message_content = True
@@ -22,15 +22,15 @@ async def get_response(user_message: str) -> str:
 
 async def send_message(message: Message, user_message: str) -> None:
     if not user_message:
-        print('(Message was empty because intents were not enabled probably)')
+        logger.info("Message was empty because intents were not enabled probably")
         return
-    if is_private := user_message[0] == '?':
+    if is_private := user_message[0] == "?":
         user_message = user_message[1:]
     try:
         response = await get_response(user_message)
         await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e:
-        print(e)
+        logger.error("Error: %s", e)
 
 
 @client.event
@@ -46,7 +46,7 @@ async def on_message(message: Message) -> None:
     user_message = message.content
     channel = str(message.channel)
 
-    logger.info("[%s] %s: \"%s\"", channel, username, user_message)
+    logger.info('[%s] %s: "%s"', channel, username, user_message)
     await send_message(message, user_message)
 
 
@@ -54,6 +54,6 @@ def main() -> None:
     client.run(token=TOKEN)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # python -m stompy.main
     main()
